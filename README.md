036284afe15b6988dd20396b81373f6a832d02b5
toyobayashi <lifenglin314@outlook.com>
2020-04-03 00:46:50 +0800

    配置文件说明

--- README.md
+++ README.md
@@ -1,3 +1,33 @@
 # Bot
 
-QQ 机器人
+一个很敷衍的 QQ 机器人。
+
+环境要求：最近版本的 Node.js
+
+``` bash
+$ git clone https://github.com/toyobayashi/bot
+$ cd ./bot
+
+$ npm install
+
+$ vi ./config.js
+
+$ npm start
+```
+
+`config.js` 配置文件自己新建哦
+
+``` js
+module.exports = {
+  accessToken: 'abc', // node server -> cqhttp 用的
+  secret: 'abc', // cqhttp -> node server 用的
+  api: 'http://127.0.0.1:5700', // cqhttp 服务
+  developerQQ: 123456789, // 开发者 QQ
+  loliconKey: 'xxx', // 不解释
+  allow: {
+    discussGroups: null, // 也可以是 number[]，null 不做限制
+    groups: null, // 也可以是 number[]，null 不做限制
+    requestFriends: null, // 也可以是 number[]，null 不做限制
+  }
+}
+```
--- default-config.js
+++ default-config.js
@@ -1,23 +1,29 @@
+const deepMerge = require('deepmerge')
+
+const defaultConfig = {
+  accessToken: '',
+  secret: '',
+  api: 'http://127.0.0.1:5700',
+  developerQQ: 0,
+  loliconKey: '',
+  allow: {
+    discussGroups: null,
+    groups: null,
+    requestFriends: null
+  }
+}
+
 let config
 try {
   config = require('./config.js')
+  config = deepMerge(defaultConfig, config)
 } catch (_) {
   try {
     config = require('./config.json')
+    config = deepMerge(defaultConfig, config)
   } catch (_) {
     console.log('未找到配置文件')
-    config = {
-      accessToken: '',
-      secret: '',
-      api: 'http://127.0.0.1:5700',
-      developerQQ: 0,
-      loliconKey: '',
-      allow: {
-        discussGroups: null,
-        groups: null,
-        requestFriends: null
-      }
-    }
+    config = defaultConfig
   }
 }
 
